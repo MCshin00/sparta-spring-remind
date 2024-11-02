b8a96a271abb7adff4aa5508e334ff7d68ef5d18
MCshin00 <sinminc7@gmail.com>
2024-11-02 22:43:17 +0900

    객체의 중복 생성 문제 해결
    스프링 입문 - 메모장 프로젝트의 IoC & DI
    
    Controller와 Service에서 객체를 중복 생성하던 코드를 생성자를 통해 객체를 생성하도록 수정

--- memo/src/main/java/com/sparta/memo/controller/MemoController.java
+++ memo/src/main/java/com/sparta/memo/controller/MemoController.java
@@ -18,33 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MemoController {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final MemoService memoService;
 
     public MemoController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoService = new MemoService(jdbcTemplate);
     }
 
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.createMemo(requestDto);
     }
 
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.getMemos();
     }
 
     @PutMapping("/memos/{id}")
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.updateMemo(id, requestDto);
     }
 
     @DeleteMapping("/memos/{id}")
     public Long deleteMemo(@PathVariable Long id) {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.deleteMemo(id);
     }
 }
