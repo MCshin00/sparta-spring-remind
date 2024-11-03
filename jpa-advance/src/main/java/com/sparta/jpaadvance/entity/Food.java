312eab32c8e0df1762dfc7b70750c61a670225bd
MCshin00 <sinminc7@gmail.com>
2024-11-04 00:17:01 +0900

    OneToOne 단방향 테스트 학습
    Spring 숙련 - 1 대 1 관계(단방향)
    
    JPA에서는 DB에서와 달리 단방향, 양방향과 같은 방향 개념이 있음. 일대일관계에서 단방향 테스트를 진행함.

---
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -0,0 +1,27 @@
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "food")
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
