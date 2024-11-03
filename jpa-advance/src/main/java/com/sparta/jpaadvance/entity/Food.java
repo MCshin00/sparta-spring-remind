fec512239236e2c7737b1509e7344b0a6805789b
MCshin00 <sinminc7@gmail.com>
2024-11-04 02:12:15 +0900

    지연 로딩과 즉시 로딩 학습
    스프링 숙련 - 지연 로딩
    
    JPA는 연관관계가 설정된 Entity의 정보를 바로 가져올지, 필요할 때 가져올지 정할 수 있다.
    LAZY는 지연 로딩으로 필요한 시점에 정보를 가져온다.
    EAGER는 즉시 로딩으로 조회할 때 연관된 모든 Entity의 정보를 즉시 가져온다.
    OneToMany는 default가 LAZY이고, ManyToOne은 EAGER이다.
    지연 로딩된 Entity의 정보를 조회하려고 할 때는 반드시 영속성 컨텍스트가 존재해야 한다. 즉, Transaction이 적용되어 있어야 한다.

--- jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -1,11 +1,14 @@
 package com.sparta.jpaadvance.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -18,6 +21,7 @@ public class Food {
     private String name;
     private double price;
 
-    @OneToMany(mappedBy = "food")
-    private List<Order> orderList = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
