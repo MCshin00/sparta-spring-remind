065cb2adfaecd725c46430d83348260c24626ba8
MCshin00 <sinminc7@gmail.com>
2024-11-03 12:39:30 +0900

    JPA Auditing 학습
    스프링 입문 - JPA Auditing 적용하기
    
    데이터의 생성 및 수정 시간은 다양한 데이터에서 자주 활용되지만, 각각의 Entity의 생성 및 수정 시간을 매번 작성하는건 비효율적이다.
    Spring Data JPA에서는 시간에 대해서 자동으로 값을 넣어주는 JPA Auditing 기능을 제공하고 있다.
    Timestamped 클래스를 abstract 클래스로 선언했다. 왜냐하면 이 클래스는 객체를 만들 일이 없기 때문이다. Entity 클래스에서 상속만 받게 할 목적이다.
    - MappedSuperclass 어노테이션은 JPA Entity 클래스들이 해당 추상 클래스를 상속할 경우 클래스에 선언한 멤버변수를 컬럼으로 인식할 수 있다.
    - EntityListeners(AuditingEntityListener.class) 어노테이션은 해당 클래스에 Auditing 기능을 포함시켜 준다.
    - CreatedDate 어노테이션은 Entity 객체가 생성되어 저장될 때 시간이 자동으로 저장된다. 최초 저장 시간은 수정할 필요가 없기 때문에 updatable=false 옵션을 추가한다.
    - LastModifiedDate 어노테이션은 조회한 Entity 객체의 값을 변경할 때마다 변경된 시간이 자동으로 저장된다.
    - Temporal은 날짜 타입을 매핑할 때 사용한다.
    - SpringBootApplication 어노테이션이 있는 클래스에 JPA Auditing 기능을 사용하겠다는 정보를 전달해주기 위해 EnableJpaAuditing 어노테이션을 추가해야 한다.

--- memo/src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ memo/src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -1,6 +1,7 @@
 package com.sparta.memo.dto;
 
 import com.sparta.memo.entity.Memo;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
@@ -8,16 +9,14 @@ public class MemoResponseDto {
     private Long id;
     private String username;
     private String contents;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public MemoResponseDto(Memo memo) {
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
-    }
-
-    public MemoResponseDto(Long id, String username, String contents) {
-        this.id = id;
-        this.username = username;
-        this.contents = contents;
+        this.createdAt = memo.getCreatedAt();
+        this.modifiedAt = memo.getModifiedAt();
     }
 }
